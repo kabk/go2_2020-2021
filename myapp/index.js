96b1d4c0d8011b05889caba5324f13684b3a482b
James Bryan Graves <jbg@JBGs.fritz.box>
2021-03-21 13:27:20 +0100

    file uploading

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   bodyParser = require('body-parser')
-  fs = require('fs').promises
+  fs = require('fs').promises,
+  fileUpload = require('express-fileupload')
 const app = express()
 const port = process.env.PORT || 3000 
 
@@ -13,6 +14,12 @@ app.use(express.static('public'))
 // We process POST body data - JBG
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
 app.use(bodyParser.json({ limit: '50mb' }))
+
+// I handle file uploads - JBG
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}))
 
 // I handle GET requests - JBG
 app.get('/', (req, res) => {
@@ -29,6 +36,18 @@ app.post('/', async (req, res) => {
     colors = req.body
     //await fs.writeFile('filename.txt', req.body.alice)
     res.send('POST request to the homepage')
+  } catch(e) {
+    res.status(500).send(e)
+  }
+})
+
+// I handle file uploads - JBG
+app.post('/files', (req, res, next) => {
+  try {
+    for(const [key, file] of Object.entries(req.files)) {
+      console.log(file.tempFilePath, file.name)
+      res.send('We got your file!')
+    }
   } catch(e) {
     res.status(500).send(e)
   }
